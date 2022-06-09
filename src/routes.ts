3f57a4323114142eb94cdfa7bb5ff89a990848b5
Daniel Ferreira <silva_120000@yahoo.com.br>
2022-06-09 06:25:44 -0300

    Salvando usuário na base de dados

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from "express";
 
+import { CreateUserController } from "./controllers/user/createUserController";
+
 const router = Router();
 
-router.get("/teste", (req: Request, res: Response) => {
-  return res.json({ ok: true });
-});
+//Rotas users
+router.post("/users", new CreateUserController().handle);
 
 export { router };
